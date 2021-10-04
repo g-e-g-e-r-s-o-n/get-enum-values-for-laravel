751b12c43b361c5d0ae45fb4f53fcc24a5039d98
Giorgi Gegetchkori <giorgi.gegetchkori.1@btu.edu.ge>
2021-10-04 18:46:10 +0400

    Update helpers.php

--- app/helpers.php
+++ app/helpers.php
@@ -3,10 +3,7 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
 
-/*
- *
- *
- */
+
 class General {
     public static function getEnumValues($table, $column) {
         $type = DB::select(DB::raw("SHOW COLUMNS FROM $table WHERE Field = '{$column}'"))[0]->Type ;
